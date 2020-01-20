2a268e150af37e614e2528cc9b56090d1a5b34c3
Diogo65 <diogo_souza97@live.com>
2020-01-19 23:18:45 -0300

    alteração de template

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -12,7 +12,7 @@ app.use('/estatico', express.static('src/app/public')); //Middeware, será execu
 app.use(bodyParser.urlencoded({ //definir o middware
     extended: true //habilitado a receber objetos complexos no formato json
 }));
-app.use(methodOverride(function (req, res) {
+app.use(methodOverride(function (req, res) { //utilizado para alternar o method header (de post p/ put)
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
       // look in urlencoded POST bodies and delete it
       var method = req.body._method;
@@ -23,5 +23,19 @@ app.use(methodOverride(function (req, res) {
 
 const rotas = require('../app/rotas/rotas.js')
 rotas(app);
+
+//Mideware para retorno da pagina 404
+app.use(function (req, resp, next) {
+  return resp.status(404).marko(
+    require ('../app/views/base/erros/404.marko')
+  );
+});
+
+//Mideware para retorno da pagina 500
+app.use(function (erro, req, resp, next) {
+  return resp.status(500).marko(
+    require ('../app/views/base/erros/500.marko')
+  );
+});
 
 module.exports  = app;
